cfe12b0514e08a881197c4bfe3a80e2115f80489
Fahid Hossain <fahidhossain2021@gmail.com>
2021-10-05 21:26:47 +0600

    page not found msg display style

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./NotFound.css"
 
 const NotFound = () => {
     return (
         <div className="error">
-            <h1>404,Page Not Found</h1>
+            <h1>Oopps!</h1>
+            <h1 className="error-heading">404-Page Not Found</h1>
+            <p>The page you are looking for might have been removed or its temporary unavaiavail</p>
+            <Link to="/home"><button className="btn btn-success">Go to HomePage</button></Link>
         </div>
     );
 };
